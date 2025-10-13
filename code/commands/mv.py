087408c8140f00ae321e3e25eb9b659a56f39155
eldar-p <139773945+eldar-p@users.noreply.github.com>
2025-10-13 22:12:36 +0300

    Create mv.py

---
+++ code/commands/mv.py
@@ -0,0 +1,11 @@
+import shutil
+
+def run(args):
+    if len(args) < 2:
+        return "mv: missing file operand"
+    src, dst = args[0], args[1]
+    try:
+        shutil.move(src, dst)
+        return ""
+    except Exception as e:
+        return f"mv: {e}"
